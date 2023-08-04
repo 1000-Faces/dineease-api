4fbee33b35e3b70caa6b3b30db1faa51d6ac138e
Akila Dharmadasa <akiladharmadasa1.1@gmail.com>
2023-08-04 22:16:54 +0530

    migrated database changes to foreign key in orders

---
+++ webapi/Migrations/20230804131546_DropRelations.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_orders_Reservation",
+                table: "orders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "FK_orders_Reservation",
+                table: "orders",
+                column: "reservation_id",
+                principalTable: "Reservation",
+                principalColumn: "reservation_id");
+        }
+    }
+}
