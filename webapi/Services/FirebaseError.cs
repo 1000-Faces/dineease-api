9b1fb160cbec340c32b53f33669a1c2ffe649879
Naveen Dharmathunga <dasheenaveen@outlook.com>
2023-10-31 12:04:51 +0530

    Added session configs

---
+++ webapi/Services/FirebaseError.cs
@@ -0,0 +1,9 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace webapi.Services
+{
+    public class FirebaseError
+    {
+        public Error error { get; set; }
+    }
+}
